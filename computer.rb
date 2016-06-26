1b4ec91d6918d181452b7c3f163b18146297f3c7
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-06-26 14:19:52 -0700

    Finish virtual computer

--- computer.rb
+++ computer.rb
@@ -1,10 +1,28 @@
 # Initialize the computer class
 class Computer
-    @@users = {}
-    def initialize(username, password)
-        @username = username
-        @password = password
-        @files = {}
-        @@users[username] = password
-    end
+  @@users = {}
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @files = {}
+    @@users[username] = password
+  end
+
+  def create(filename)
+    time = Time.now
+    @files = {}
+    @files[filename] = time
+    puts "User #{@username} created #{filename} at #{time}"
+  end
+
+  def Computer.get_users
+    puts @@users
+  end
 end
+
+my_computer = Computer.new('Moose', 'Julia')
+
+Computer.get_users
+
+my_computer.create('codecademy')
+
