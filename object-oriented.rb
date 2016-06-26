edb080b4ea25ff5cc2b48160ccc563221bba19aa
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-06-26 13:27:01 -0700

    playing with classes

--- object-oriented.rb
+++ object-oriented.rb
@@ -51,3 +51,21 @@ end
 
 # puts dragon.say_my_name
 # puts felines.fight
+
+# -----------------------------
+class Message
+    @@messages_sent = 0
+    def initialize(from, to)
+        @from = from
+        @to = to
+        @@messages_sent += 1
+    end
+end
+
+my_message = Message.new('Moose', 'Vy')
+
+class Email < Message
+    def initialize(from, to)
+        super
+    end
+end
