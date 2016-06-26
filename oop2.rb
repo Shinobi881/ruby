1cd16d32d4e303ef43223f545d296d2696409b0e
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-06-26 14:56:10 -0700

    Public/privagte methods

--- oop2.rb
+++ oop2.rb
@@ -1,6 +1,28 @@
 class Dog 
-   def initialize(name, breed)
-       @name = name
-       @breed = breed
-   end
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+
+  public
+
+  def bark
+    puts 'Woof!'
+  end
+
+  def whine
+    puts 'harrrf harrrf'
+  end
+  private
+
+  def howl
+    puts 'howl'
+  end
+
 end
+
+bruno = Dog.new('Bruno', 'Bulldog')
+
+# bruno.bark
+
+bruno.whine
