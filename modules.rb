10434cd969a76814c3312f4f1bf7a87a8eb0b05c
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-06-26 15:52:54 -0700

    Including libraries

--- modules.rb
+++ modules.rb
@@ -1,5 +1,23 @@
 module MyLib
   MY_CONSTANT = 'BBoy'
+
+  def lib_out
+    puts 'Put my library out there'
+  end
 end
 
-puts MyLib::MY_CONSTANT
+# puts MyLib::MY_CONSTANT
+
+class Test
+  include MyLib
+  def initialize
+  end
+
+  def let_out
+    lib_out
+    puts MY_CONSTANT
+  end
+end
+
+test = Test.new
+test.let_out
