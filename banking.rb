c65ff4ee67062b7f529786838ab43d997be70b04
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-06-26 16:21:07 -0700

    Withdraw method

--- banking.rb
+++ banking.rb
@@ -25,4 +25,13 @@ class Account
       puts pin_error
     end
   end
+
+  def withdraw(pin_number, amount)
+    if pin_number = @pin
+      @balance -= amount
+      puts "Withdrew $#{amount}. New balance: $#{@balance}"
+    else
+      puts pin_error
+    end    
+  end
 end
